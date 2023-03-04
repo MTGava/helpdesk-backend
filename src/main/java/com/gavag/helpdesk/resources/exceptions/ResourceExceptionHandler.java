619d38db75af9913de53f265c00f107eb72ee2fd
Matheus Gava <matheus.gava3@gmail.com>
2023-03-04 13:18:41 -0300

    Feat: Excecao para CPF e EMAIL ja cadastrados

--- src/main/java/com/gavag/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/gavag/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.gavag.helpdesk.resources.exceptions;
 
+import com.gavag.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.gavag.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +13,18 @@ import javax.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request) {
+    public ResponseEntity<StandardError> handleException(ObjectNotFoundException ex, HttpServletRequest request) {
 
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
                 "Object Not Found", ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> handleException(DataIntegrityViolationException ex, HttpServletRequest request) {
+
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Violação de dados", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/gavag/helpdesk/services/TecnicoService.java
+++ src/main/java/com/gavag/helpdesk/services/TecnicoService.java
@@ -1,8 +1,11 @@
 package com.gavag.helpdesk.services;
 
+import com.gavag.helpdesk.domain.Pessoa;
 import com.gavag.helpdesk.domain.Tecnico;
 import com.gavag.helpdesk.domain.dtos.TecnicoDTO;
+import com.gavag.helpdesk.repositories.PessoaRepository;
 import com.gavag.helpdesk.repositories.TecnicoRepository;
+import com.gavag.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.gavag.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,9 @@ public class TecnicoService {
     @Autowired
     private TecnicoRepository repository;
 
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
     public Tecnico findById(Integer id) {
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Técnico com o ID: " + id + " não encontrado!"));
@@ -27,7 +33,19 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO objDTO) {
         objDTO.setId(null);
+        validaPorCpfEEmail(objDTO);
         Tecnico newObj = new Tecnico(objDTO);
         return repository.save(newObj);
+    }
+
+    private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+        }
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+            throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+        }
     }
 }
