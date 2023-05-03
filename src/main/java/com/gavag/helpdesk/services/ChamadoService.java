29d4e209ac0f7c864e9f7b01e9b3dd3b662e170b
Matheus Gava <matheus.gava3@gmail.com>
2023-05-03 13:03:30 -0300

    Feat: ChamadoResource

---
+++ src/main/java/com/gavag/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.gavag.helpdesk.services;
+
+import com.gavag.helpdesk.domain.Chamado;
+import com.gavag.helpdesk.repositories.ChamadoRepository;
+import com.gavag.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado com o ID: " + id + " não encontrado!"));
+    }
+
+}
