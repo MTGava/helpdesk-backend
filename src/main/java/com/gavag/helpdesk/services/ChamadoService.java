42e42681f421ddb4240efc3df1093781116b0e05
Matheus Gava <matheus.gava3@gmail.com>
2023-05-03 13:45:21 -0300

    Feat: Chamado findAll

--- src/main/java/com/gavag/helpdesk/services/ChamadoService.java
+++ src/main/java/com/gavag/helpdesk/services/ChamadoService.java
@@ -6,6 +6,7 @@ import com.gavag.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,4 +20,7 @@ public class ChamadoService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado com o ID: " + id + " não encontrado!"));
     }
 
+    public List<Chamado> findAll() {
+        return repository.findAll();
+    }
 }
