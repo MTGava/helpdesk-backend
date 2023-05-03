0fbe6b92022a06fea1f485f4eb188488fba76034
Matheus Gava <matheus.gava3@gmail.com>
2023-05-03 15:06:54 -0300

    Feat: Chamado - create

--- src/main/java/com/gavag/helpdesk/services/ChamadoService.java
+++ src/main/java/com/gavag/helpdesk/services/ChamadoService.java
@@ -1,6 +1,11 @@
 package com.gavag.helpdesk.services;
 
 import com.gavag.helpdesk.domain.Chamado;
+import com.gavag.helpdesk.domain.Cliente;
+import com.gavag.helpdesk.domain.Tecnico;
+import com.gavag.helpdesk.domain.dtos.ChamadoDTO;
+import com.gavag.helpdesk.domain.enums.Prioridade;
+import com.gavag.helpdesk.domain.enums.Status;
 import com.gavag.helpdesk.repositories.ChamadoRepository;
 import com.gavag.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +20,12 @@ public class ChamadoService {
     @Autowired
     private ChamadoRepository repository;
 
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @Autowired
+    private ClienteService clienteService;
+
     public Chamado findById(Integer id) {
         Optional<Chamado> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado com o ID: " + id + " não encontrado!"));
@@ -22,5 +33,27 @@ public class ChamadoService {
 
     public List<Chamado> findAll() {
         return repository.findAll();
+    }
+
+    public Chamado create(ChamadoDTO objDTO) {
+        return repository.save(newChamado(objDTO));
+    }
+
+    private Chamado newChamado(ChamadoDTO obj) {
+        Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+        Cliente cliente = clienteService.findById(obj.getCliente());
+
+        Chamado chamado = new Chamado();
+        if (obj.getId() != null) {
+            chamado.setId(obj.getId());
+        }
+
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+        chamado.setStatus(Status.toEnum(obj.getStatus()));
+        chamado.setTitulo(obj.getTitulo());
+        chamado.setObservacoes(obj.getObservacoes());
+        return chamado;
     }
 }
